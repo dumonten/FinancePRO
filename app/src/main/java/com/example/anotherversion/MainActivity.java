b44f07079c52bbd95b3fadef6aaa03d2b4e8680e
AlabaAclydiem <pastukhou.kirill@gmail.com>
2021-10-31 19:06:29 +0300

    Imaginary

--- app/src/main/java/com/example/anotherversion/MainActivity.java
+++ app/src/main/java/com/example/anotherversion/MainActivity.java
@@ -1,25 +1,63 @@
 package com.example.anotherversion;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnStat;
+    Button btnStat, btnDay, btnWeek, btnMonth;
+    Dialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        dialog = new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.dialog_intervals);
+
         btnStat = (Button) findViewById(R.id.toTime);
+        btnDay = (Button) dialog.findViewById(R.id.bDay);
+        btnWeek = (Button) dialog.findViewById(R.id.bWeek);
+        btnMonth = (Button) dialog.findViewById(R.id.bMonth);
 
         btnStat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), TimeIntervals.class);
+                dialog.show();
+            }
+        });
+
+        btnDay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You've chosen 'Day'", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), StatisticsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnWeek.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You've chosen 'Week'", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), StatisticsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnMonth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You've chosen 'Month'", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), StatisticsActivity.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/anotherversion/StatisticsActivity.java
@@ -0,0 +1,15 @@
+package com.example.anotherversion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StatisticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics);
+        getSupportActionBar().setTitle(" ");
+    }
+}
