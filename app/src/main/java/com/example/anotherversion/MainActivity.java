84230256a5386930760dca575fb13370e8f10e91
AlabaAclydiem <pastukhou.kirill@gmail.com>
2021-10-31 16:12:30 +0300

    TimeIntervals Through Activity

--- app/src/main/java/com/example/anotherversion/MainActivity.java
+++ app/src/main/java/com/example/anotherversion/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.anotherversion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnStat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStat = (Button) findViewById(R.id.toTime);
+
+        btnStat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), TimeIntervals.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/anotherversion/TimeIntervals.java
@@ -0,0 +1,15 @@
+package com.example.anotherversion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimeIntervals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_intervals);
+        getSupportActionBar().setTitle(" ");
+    }
+}
